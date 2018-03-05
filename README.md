c81b5067018607c4e0abf386037fe160afc60584
bsehovac <bsehovac@gmail.com>
2018-03-05 09:46:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# CSS Pixels To Rem
+
+A sublime text plugin that converts pixels CSS units to rem and back.
+
+## Installation
+
+```bash
+cd "~/Library/Application Support/Sublime Text 3/Packages"
+git clone https://github.com/cruncher/CSSPxToRem.git
+```
+
+or
+
+- Download and unzip this repository on your computer.
+- Make sure the name of the unzipped folder is **CSSPxToRem**.
+- Select `Sublime Text > Preferences > Browse Packages...` in the menu.
+- Copy **CSSPxToRem** folder to your **/packages/** directory of Sublime Text.
+
+## Usage
+
+- Select a block of CSS.
+- Hit (⌘/Ctrl + R) to convert all selected CSS values from pixels to rems.
+- Hit (⌘/Ctrl + ⇧ + R) to convert all selected CSS values from rems to pixels.
+- If the selection is empty, the conversion is applied to the current line.
+
+## Credits
+Based on [cruncher](https://github.com/cruncher/) package [CSSPxToRem](https://github.com/cruncher/CSSPxToRem).
---
+++ css_px_to_rem.py
@@ -0,0 +1,62 @@
+import sublime
+import sublime_plugin
+import re
+
+class PxToRemCommand(sublime_plugin.TextCommand):
+
+    def __init__(self, *args, **kwargs):
+        super(PxToRemCommand, self).__init__(*args, **kwargs)
+        self.rem_height = sublime.load_settings('CSSPxToRem.sublime-settings').get('rem_height', 16)
+
+    def run(self, edit):
+        for s in self.view.sel():
+            if s.empty():
+                s = self.view.line(s)
+
+            txt = self.transform(self.view.substr(s))
+            self.view.replace(edit, s, txt)
+
+    def transform(self, str):
+        def repl(m):
+            gd = m.groupdict()
+
+            # px -> rem
+            if gd.get('unit', None) == 'px':
+                ret = '%3.4f' % (float(gd.get('qty')) / float(self.rem_height))
+                while ret[-1] == '0':
+                    ret = ret[:-1]
+                if ret.endswith('.'):
+                    ret = ret[:-1]
+                return ret + 'rem'
+
+        return re.sub(r'((?P<qty>[0-9\.]+)(?P<unit>px))', repl, str)
+
+class RemToPxCommand(sublime_plugin.TextCommand):
+
+    def __init__(self, *args, **kwargs):
+        super(RemToPxCommand, self).__init__(*args, **kwargs)
+        self.rem_height = sublime.load_settings('CSSPxToRem.sublime-settings').get('rem_height', 16)
+
+    def run(self, edit):
+        for s in self.view.sel():
+            if s.empty():
+                s = self.view.line(s)
+
+            txt = self.transform(self.view.substr(s))
+            self.view.replace(edit, s, txt)
+
+    def transform(self, str):
+        def repl(m):
+            gd = m.groupdict()
+
+            # rem -> px
+            if gd.get('unit', None) == 'rem':
+                ret_val = float(gd.get('qty')) * float(self.rem_height)
+                ret = '%3.1f' % ret_val
+                if ret.endswith('.0'):
+                    ret = ('%3.0f' % ret_val) + 'px'
+                else:
+                    ret = ret + 'px'
+                return ret.strip()
+
+        return re.sub(r'((?P<qty>[0-9\.]+)(?P<unit>rem))', repl, str)
